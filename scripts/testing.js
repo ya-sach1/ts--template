d66ac86a72ea0d6dacdbc1f0bd0f2dc648d86340
ya-sach1 <ikurogane85@gmail.com>
2021-07-23 10:57:20 +0200

    🐛 I may be dumb

--- scripts/testing.js
+++ scripts/testing.js
@@ -3,7 +3,7 @@
 const { execSync } = require('child_process');
 const { writeFileSync, mkdirSync } = require('fs');
 const pkg = require('../package.json');
-const { path, ifEmptyDelete } = require('./shared');
+const { path, ifEmptyDelete, rmSync } = require('./shared');
 
 pkg.scripts.test = 'yarn run prestart && jest';
 pkg.scripts['test:coverage'] = 'yarn run prestart && jest --coverage';
@@ -11,8 +11,8 @@ pkg.scripts['test:coverage'] = 'yarn run prestart && jest --coverage';
 writeFileSync(path('package.json'), JSON.stringify(pkg, null, 4));
 writeFileSync(
 	path('jest.config.js'),
-	JSON.stringify(
-		{
+`
+	module.exports =	{
 			verbose: true,
 			moduleFileExtensions: ['ts', 'js'],
 			preset: 'ts-jest',
@@ -21,13 +21,11 @@ writeFileSync(
 			testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
 			collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 		},
-		null,
-		4,
-	),
+`
 );
 
 mkdirSync(path('tests'));
 console.log('Installing packages... do not quit.');
-execSync(`yarn add -D ts-jest typescript jest ${pkg.devDependencies.eslint && !pkg.devDependencies['eslint-plugin-jest'] ? 'eslint-plugin-jest' : ''}`);
+execSync(`yarn add -D ts-jest @types/jest typescript jest ${pkg.devDependencies.eslint && !pkg.devDependencies['eslint-plugin-jest'] ? 'eslint-plugin-jest' : ''}`);
 rmSync(path('scripts', 'testing.js'));
 ifEmptyDelete();
